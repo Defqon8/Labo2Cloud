817ed92388a08b4c11040e0fda2a7934d05efd9d
Defqon8 <bram.robyn@student.howest.be>
2018-10-01 17:52:37 +0200

    Calculator added

---
+++ FunctionApp1/FunctionApp1/calculator.cs
@@ -0,0 +1,26 @@
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionApp1
+{
+    public static class calculator
+    {
+        [FunctionName("calculator")]
+        public static async Task<IActionResult>Calc([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "calculator/som/{number1}/{number2}")]HttpRequest req,int number1, int number2, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            int som = number1 + number2;
+            return new OkObjectResult(som);
+            
+        }
+    }
+}
