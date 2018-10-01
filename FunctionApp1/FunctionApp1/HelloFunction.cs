a7bf58c536944a274aa5fb9a6eedb2f43824d40c
Defqon8 <bram.robyn@student.howest.be>
2018-10-01 17:23:52 +0200

    2nd function

--- FunctionApp1/FunctionApp1/HelloFunction.cs
+++ FunctionApp1/FunctionApp1/HelloFunction.cs
@@ -29,5 +29,21 @@ namespace FunctionApp1
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
+
+        [FunctionName("TestParams")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "naam/{myname}")]HttpRequest req, String myname, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
     }
 }
