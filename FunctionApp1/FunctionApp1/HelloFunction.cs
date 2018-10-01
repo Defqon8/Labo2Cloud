af3cafd94bf6ecef5e9b3d7e3425690c457d3a28
Defqon8 <bram.robyn@student.howest.be>
2018-10-01 17:33:51 +0200

    fix2

--- FunctionApp1/FunctionApp1/HelloFunction.cs
+++ FunctionApp1/FunctionApp1/HelloFunction.cs
@@ -34,14 +34,7 @@ namespace FunctionApp1
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "naam/{name}")]HttpRequest req, String name, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return new OkObjectResult("Hello, " + name);
         }
     }
 }
